abc7fa2c9c326cc303e66385525ea7a0581a7bcc
parksangyong <qkr152369@gmail.com>
2023-10-22 00:46:55 +0900

    yml로 변환하여 등록

---
+++ src/main/java/com/example/account/exam/AnnoExample.java
@@ -0,0 +1,32 @@
+package com.example.account.exam;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+//@NoArgsConstructor
+//arg를 없애줌, AllArgsConstructor과 같이 사용 불가
+@AllArgsConstructor
+//한번에 arg를 설정할 때 사용 테스트(commend+shift+t)확인
+@RequiredArgsConstructor
+//
+//@Data
+//getter,setter,Tostring,RequiredArgsConstructor를 한번에 설정
+//보안에 문제 있을 수 있어 가능하면 필요한것 하나씩 생성
+@Slf4j
+//log
+//@UtilityClass
+//생성자 자동 생성
+public class AnnoExample {
+    private String accountNumber;
+    private String nickname;
+    private LocalDateTime registeredAt;
+
+    public void log(){
+        log.error("error is occurred");
+    }
+}
---
+++ src/test/java/com/example/account/exam/AnnoExampleTest.java
@@ -0,0 +1,28 @@
+package com.example.account.exam;
+
+import com.example.account.exam.AnnoExample;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+class AnnoExampleTest {
+    @Test
+    void accountDto(){
+        //given
+        //when
+        //then
+
+        AnnoExample annoexample = new AnnoExample(
+                "accountNumber",
+                "summer",
+                LocalDateTime.now()
+        );
+        //@AllArgs~ 가 있다면 한번에 선언 가능
+        //설정안하시 밑의 설정 해야함
+        //accountDto.setAccountNumber("accountNumber");
+        //accountDto.setNickname("summer");
+
+        System.out.println(annoexample.getAccountNumber());
+        System.out.println(annoexample.toString());
+    }
+}
