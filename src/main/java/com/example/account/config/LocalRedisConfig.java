d891103f7d20db638e15aebdbd13472062f6d980
parksangyong <qkr152369@gmail.com>
2023-10-22 03:33:07 +0900

    @value설정부터다시

--- src/main/java/com/example/account/config/LocalRedisConfig.java
+++ src/main/java/com/example/account/config/LocalRedisConfig.java
@@ -1,30 +1,9 @@
 package com.example.account.config;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Value;
 import org.springframework.context.annotation.Configuration;
-import redis.embedded.RedisServer;
 
-//@Configuration
-//public class LocalRedisConfig {
-//
-//    @Value("${spring.redis.port}")
-//    private int redisPort;
-//
-//    private RedisServer redisServer;
-//
-//    @PostConstruct
-//    public void startRedis(){
-//        redisServer = new RedisServer(redisPort);
-//        redisServer.start();
-//    }
-//
-//    @PreDestroy
-//    public void stopRedis(){
-//        if (redisServer != null){
-//            redisServer.stop();
-//        }
-//    }
-//}
+@Configuration
+public class LocalRedisConfig {
+    @Value("${spring.redis.port}")
+}
