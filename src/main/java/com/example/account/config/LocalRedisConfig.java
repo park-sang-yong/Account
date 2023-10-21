ea1b0297c45bc894198eafa0d122e83534a6c256
parksangyong <qkr152369@gmail.com>
2023-10-22 03:38:33 +0900

    @value fix

--- src/main/java/com/example/account/config/LocalRedisConfig.java
+++ src/main/java/com/example/account/config/LocalRedisConfig.java
@@ -1,9 +1,10 @@
 package com.example.account.config;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LocalRedisConfig {
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
 }
